f9ee1a861b36ccd21489c24f41bea0d693e22d7c
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-04-25 18:23:21 +0900

    2-3-3 演習3 User のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
+# coding: utf-8
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :microposts
+  validates name, presence: true # 「FILL_IN」をコードに置き換えてください
+  validates email, presence: true # 「FILL_IN」をコードに置き換えてください
 end
